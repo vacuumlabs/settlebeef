40c6f137931b8fe52066a37be1093dcfa79600e4
Matej Poklemba <matejpoklemba@gmail.com>
2024-05-10 20:03:42 +0200

    Add New beef button to navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Stack, Toolbar, Typography } from "@mui/material";
 import React from "react";
 import LoginButton from "./LoginButton";
 import Link from "next/link";
@@ -7,15 +7,29 @@ const Header = () => {
   return (
     <AppBar>
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Link href="/" style={{ textDecoration: "none" }}>
-          <Typography
-            variant="h6"
-            component="span"
-            sx={{ flexGrow: 1, color: "white" }}
+        <Stack sx={{ flexDirection: "row" }}>
+          <Link
+            href="/"
+            style={{ textDecoration: "none", marginRight: "48px" }}
           >
-            Decobie
-          </Typography>
-        </Link>
+            <Typography
+              variant="h6"
+              component="span"
+              sx={{ flexGrow: 1, color: "white" }}
+            >
+              Decobie
+            </Typography>
+          </Link>
+          <Link href="/beef/new" style={{ textDecoration: "none" }}>
+            <Typography
+              variant="h6"
+              component="span"
+              sx={{ flexGrow: 1, color: "white" }}
+            >
+              New beef
+            </Typography>
+          </Link>
+        </Stack>
         <LoginButton />
       </Toolbar>
     </AppBar>
