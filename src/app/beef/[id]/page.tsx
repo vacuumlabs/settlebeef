e26a638213f598495237b98013fe6a42dd623804
Matej Poklemba <matejpoklemba@gmail.com>
2024-05-11 00:16:30 +0200

    Fix deadline

--- src/app/beef/[id]/page.tsx
+++ src/app/beef/[id]/page.tsx
@@ -102,7 +102,7 @@ const BeefDetailPage = ({ params }: BeefDetailPageProps) => {
   const beef = useBeef(id);
   const arbiterStatuses = useGetArbiterStatuses(
     (beef?.address ?? "0x0") as Address,
-    beef?.arbiters ?? [],
+    beef?.arbiters ?? []
   );
 
   if (beef === undefined) {
@@ -168,14 +168,16 @@ const BeefDetailPage = ({ params }: BeefDetailPageProps) => {
         step = 4;
         // TODO: this assumes constant settlingDuration of 30 days!
         deadline = new Date(
-          Number(settleStart + BigInt(60 * 60 * 24 * 30)) * 1000,
+          Number(settleStart + BigInt(60 * 60 * 24 * 30)) * 1000
         );
         if (resultYes > arbiters.length / 2 || resultNo > arbiters.length / 2) {
           step = 5;
+          deadline = undefined;
           if (/*served*/ false) {
             step = 6;
           }
         } else if (now > (settleStart + BigInt(60 * 60 * 24 * 30)) * 1000n) {
+          deadline = undefined;
           // TODO: this assumes constant settlingDuration of 30 days!
           steps = steps.slice(0, 5);
           steps.push({ icon: "🤢", text: "Beef rotten" });
