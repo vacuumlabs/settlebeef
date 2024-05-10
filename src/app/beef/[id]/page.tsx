ff6139f7fd2d7484553bcfbf9871cad6c9deeca3
Matej Poklemba <matejpoklemba@gmail.com>
2024-05-11 00:13:19 +0200

    Fixed steps logic (hopefully 🤞)

--- src/app/beef/[id]/page.tsx
+++ src/app/beef/[id]/page.tsx
@@ -165,25 +165,21 @@ const BeefDetailPage = ({ params }: BeefDetailPageProps) => {
         deadline = new Date(Number(settleStart) * 1000);
         step = 3;
       } else {
-        if (now < (settleStart + BigInt(60 * 60 * 24 * 30)) * 1000n) {
-          // TODO: this assumes constant settlingDuration of 30 days!
-          deadline = new Date(
-            Number(settleStart + BigInt(60 * 60 * 24 * 30)) * 1000
-          );
-          step = 4;
-        } else {
+        step = 4;
+        // TODO: this assumes constant settlingDuration of 30 days!
+        deadline = new Date(
+          Number(settleStart + BigInt(60 * 60 * 24 * 30)) * 1000
+        );
+        if (resultYes > arbiters.length / 2 || resultNo > arbiters.length / 2) {
           step = 5;
-          if (
-            resultYes <= arbiters.length / 2 &&
-            resultNo <= arbiters.length / 2
-          ) {
-            steps = steps.slice(0, 5);
-            steps.push({ icon: "🤢", text: "Beef rotten" });
-            isRotten = true;
-          }
           if (/*served*/ false) {
             step = 6;
           }
+        } else if (now > (settleStart + BigInt(60 * 60 * 24 * 30)) * 1000n) {
+          // TODO: this assumes constant settlingDuration of 30 days!
+          steps = steps.slice(0, 5);
+          steps.push({ icon: "🤢", text: "Beef rotten" });
+          isRotten = true;
         }
       }
     }
