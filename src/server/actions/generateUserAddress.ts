dbefe335814c532b5f5d62e43d5c9c988f872330
Matej Falat <matej.falat@gmail.com>
2024-05-10 12:28:41 +0200

    generate addresses in advance

---
+++ src/server/actions/generateUserAddress.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import {
+  LinkedAccountWithMetadata,
+  PrivyClient,
+  User,
+  WalletWithMetadata,
+} from "@privy-io/server-auth";
+
+const privy = new PrivyClient(
+  process.env.NEXT_PUBLIC_PRIVY_APP_ID!,
+  process.env.PRIVY_APP_SECRET!
+);
+
+const isWalletWithMetadata = (
+  account: LinkedAccountWithMetadata
+): account is WalletWithMetadata =>
+  account.type === "wallet" && account.walletClientType === "privy";
+
+const getSignerAddress = (user: User | null) => {
+  const account = user?.linkedAccounts.find(isWalletWithMetadata);
+  return account?.address;
+};
+
+export type GenerateUserAddressParams = Parameters<
+  typeof privy.importUser
+>[0]["linkedAccounts"];
+
+export const generateUserAddress = async (
+  params: GenerateUserAddressParams
+) => {
+  const user = await privy.importUser({
+    linkedAccounts: params,
+    createEmbeddedWallet: true,
+  });
+
+  return {
+    user,
+    signerAddress: getSignerAddress(user),
+  };
+};
---
+++ src/utils/generateParams.ts
@@ -0,0 +1,26 @@
+import {
+  GenerateUserAddressParams,
+  generateUserAddress,
+} from "@/server/actions/generateUserAddress";
+import { getKernelAddressFromECDSA } from "@zerodev/ecdsa-validator";
+import { ENTRYPOINT_ADDRESS_V07 } from "permissionless";
+import { isAddress } from "viem";
+import { publicClient } from "./chain";
+
+export const getUserGeneratedAddress = async (
+  params: GenerateUserAddressParams,
+  index = 0
+) => {
+  const { signerAddress } = await generateUserAddress(params);
+
+  if (!signerAddress || !isAddress(signerAddress)) {
+    throw new Error("Failed to pregenerate address");
+  }
+
+  return getKernelAddressFromECDSA({
+    eoaAddress: signerAddress,
+    index: BigInt(index),
+    entryPointAddress: ENTRYPOINT_ADDRESS_V07,
+    publicClient: publicClient,
+  });
+};
