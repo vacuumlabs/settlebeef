7db7fa668cf7427719efb7a108b0fe2739780856
Matej Falat <matej.falat@gmail.com>
2024-05-10 11:32:24 +0200

    add wagmi config

--- src/components/providers/Providers.tsx
+++ src/components/providers/Providers.tsx
@@ -3,28 +3,47 @@
 import { PrivyProvider } from "@privy-io/react-auth";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SmartAccountClientContextProvider } from "./SmartAccountClientContext";
+import { http } from "viem";
+import { activeChain } from "@/utils/chain";
+import { injected } from "wagmi/connectors";
+import { WagmiProvider, createConfig } from "wagmi";
 
 type ProvidersProps = {
   children: React.ReactNode;
 };
 
-const queryClient = new QueryClient();
-
 const Providers = ({ children }: ProvidersProps) => {
+  const queryClient = new QueryClient();
+
+  const zdAppId = process.env.NEXT_PUBLIC_ZERODEV_APP_ID || "";
+  const config = createConfig({
+    chains: [activeChain],
+    connectors: [injected()],
+    transports: {
+      [activeChain.id]: http(
+        `https://rpc.zerodev.app/api/v2/bundler/${zdAppId}`
+      ),
+    },
+  });
+
   return (
     <QueryClientProvider client={queryClient}>
-      <PrivyProvider
-        appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
-        config={{
-          embeddedWallets: {
-            createOnLogin: "users-without-wallets",
-          },
-        }}
-      >
-        <SmartAccountClientContextProvider>
-          {children}
-        </SmartAccountClientContextProvider>
-      </PrivyProvider>
+      <WagmiProvider config={config}>
+        <PrivyProvider
+          appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
+          config={{
+            embeddedWallets: {
+              createOnLogin: "users-without-wallets",
+            },
+            defaultChain: activeChain,
+            supportedChains: [activeChain],
+          }}
+        >
+          <SmartAccountClientContextProvider>
+            {children}
+          </SmartAccountClientContextProvider>
+        </PrivyProvider>
+      </WagmiProvider>
     </QueryClientProvider>
   );
 };
