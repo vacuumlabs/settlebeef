928e656d9be37ea196beb50a860e8fba9c40d6fb
Samuel Šulovský <samuel.sulovsky@vacuumlabs.com>
2024-05-10 20:20:01 +0200

    Add balance to header 💯

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { Button, Stack, Typography } from "@mui/material";
+import { Box, Button, Skeleton, Stack, Typography } from "@mui/material";
 import { useLogin, useLogout, usePrivy } from "@privy-io/react-auth";
 import { useContext } from "react";
 import { SmartAccountClientContext } from "./providers/SmartAccountClientContext";
+import { useBalance } from "wagmi";
+import { formatEther } from "viem";
 
 const LoginButton = () => {
   const { authenticated } = usePrivy();
@@ -17,11 +19,28 @@ const LoginButton = () => {
     },
   });
 
+  const { data: balance, isLoading } = useBalance({
+    address: client?.account.address,
+  });
+
   return authenticated ? (
     <Stack direction="row" alignItems="center" gap={3}>
-      <Typography component="span" variant="body2">
-        {client?.account.address}
-      </Typography>
+      {client == null ? (
+        <Skeleton variant="circular" />
+      ) : (
+        <Stack direction="row" gap={3}>
+          {isLoading || balance == null ? (
+            <Skeleton variant="circular" />
+          ) : (
+            <Typography component="span" variant="body2">
+              {formatEther(BigInt(balance.value.toString()))} ETH
+            </Typography>
+          )}
+          <Typography component="span" variant="body2">
+            {client?.account.address}
+          </Typography>
+        </Stack>
+      )}
       <Button color="inherit" onClick={logout}>
         Logout
       </Button>
