c818b568c2a3a2fdb0a023ce0d65677a0e8c85d8
Matej Poklemba <matejpoklemba@gmail.com>
2024-05-10 12:36:12 +0200

    Let it build 🔨

--- contracts/test/Beef.t.sol
+++ contracts/test/Beef.t.sol
@@ -8,6 +8,14 @@ contract BeefTest is Test {
     Beef public beef;
 
     function setUp() public {
-        beef = new Beef(address(this));
+        Beef.ConstructorParams memory params = Beef.ConstructorParams({
+            owner: address(this),
+            wager: 1000,
+            foe: address(0),
+            duration: 30 days,
+            title: "Test Beef",
+            description: "This is a test beef."
+        });
+        beef = new Beef(params);
     }
 }
