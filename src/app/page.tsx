647c86e037b4985d6d1ecf3d009a904c8ebe3c5a
Samuel Šulovský <samuel.sulovsky@vacuumlabs.com>
2024-05-10 13:48:19 +0200

    Barebones essentials setup 🦴👷

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+
+import { SmartAccountClientContext } from "@/components/providers/SmartAccountClientContext";
 import { Box, Typography } from "@mui/material";
+import { useContext } from "react";
 
 export default function Home() {
-  return (
-    <Box mt={8}>
-      <Typography variant="h1">Home</Typography>
-    </Box>
-  );
+        const { client } = useContext(SmartAccountClientContext);
+        return (
+                <Box mt={8}>
+                        <Typography variant="h1">Home</Typography>
+                        <Typography variant="body1">
+                                {client ? "Connected" : "Not connected"}
+                                {client && JSON.stringify(client.account)}
+                        </Typography>
+
+                </Box>
+        );
 }
