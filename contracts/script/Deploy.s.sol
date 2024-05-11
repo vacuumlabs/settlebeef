f63202df17b12baaa78dd462aa31a3c04dc0220f
Matej Poklemba <matejpoklemba@gmail.com>
2024-05-11 12:12:51 +0200

    STAKING 🥩

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -9,9 +9,13 @@ contract Deploy is Script {
     function setUp() public {}
 
     function run() public {
+        address WETH = vm.envAddress("WETH_ADDRESS");
+        address WSTETH = vm.envAddress("WSTETH_ADDRESS");
+        address uniswapV2Router = vm.envAddress("UNIV2ROUTER_ADDRESS");
         vm.broadcast();
 
-        Slaughterhouse slaughterhouse = new Slaughterhouse();
+        Slaughterhouse slaughterhouse = new Slaughterhouse(WETH, WSTETH, uniswapV2Router);
         console2.log("Slaughterhouse:", address(slaughterhouse));
+        console2.log("Beef implementation:", slaughterhouse.beefImplementation());
     }
 }
--- contracts/src/Slaughterhouse.sol
+++ contracts/src/Slaughterhouse.sol
@@ -8,20 +8,26 @@ import {Beef} from "./Beef.sol";
 contract Slaughterhouse {
     address public beefImplementation;
     address[] public beefs;
+    address public WETH;
+    address public WSTETH;
+    address public uniswapV2Router;
 
     event BeefPackaged(address indexed beef, address indexed owner, address indexed foe);
 
-    constructor() {
+    constructor(address _weth, address _wsteth, address _uniswapV2Router) {
         beefImplementation = address(new Beef());
+        WETH = _weth;
+        WSTETH = _wsteth;
+        uniswapV2Router = _uniswapV2Router;
     }
 
     function getBeefs() public view returns (address[] memory) {
         return beefs;
     }
 
-    function packageBeef(Beef.ConstructorParams memory params) public payable returns (address) {
-        address beef = Clones.clone(beefImplementation);
-        Beef(beef).initialize{value: msg.value}(params);
+    function packageBeef(Beef.ConstructorParams memory params, uint256 amountOutMin) public payable returns (address) {
+        address payable beef = payable(Clones.clone(beefImplementation));
+        Beef(beef).initialize{value: msg.value}(params, amountOutMin, WETH, WSTETH, uniswapV2Router);
         beefs.push(beef);
         emit BeefPackaged(beef, params.owner, params.foe);
         return beef;
