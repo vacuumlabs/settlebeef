d4269a60380f303860194c1c76b8f4070965ee0e
SJakabcin <100082184+sebastianj21@users.noreply.github.com>
2024-05-30 13:49:16 +0200

    Header: Use gap instead of margin
    Also remove unnecessary `flexGrow`

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,17 +6,12 @@ import Link from "next/link";
 const Header = () => {
   return (
     <AppBar sx={{ borderRadius: 0 }}>
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+      <Toolbar
+        sx={{ display: "flex", justifyContent: "space-between", gap: "48px" }}
+      >
         <Stack sx={{ flexDirection: "row" }}>
-          <Link
-            href="/"
-            style={{ textDecoration: "none", marginRight: "48px" }}
-          >
-            <Typography
-              variant="h3"
-              component="span"
-              sx={{ flexGrow: 1, color: "white" }}
-            >
+          <Link href="/" style={{ textDecoration: "none" }}>
+            <Typography variant="h3" component="span" sx={{ color: "white" }}>
               🥩SettleBeef
             </Typography>
           </Link>
