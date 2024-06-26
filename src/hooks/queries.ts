0d69f592d02b52af8d6c7d92f62bfdf69733f730
SJakabcin <100082184+sebastianj21@users.noreply.github.com>
2024-06-26 16:01:57 +0200

    Support showing usernames from socials (twitter, email, ...)

--- src/hooks/queries.ts
+++ src/hooks/queries.ts
@@ -6,22 +6,25 @@ import { BeefSortType } from "@/components/BeefSortDropdown"
 import { ensConfig } from "@/components/providers/Providers"
 import { SmartAccountClientContext } from "@/components/providers/SmartAccountClientContext"
 import { BeefApi } from "@/server/actions/beef/beefApi"
+import { getAllUserDetailsAction } from "@/server/actions/getUserDetailsAction"
+import { Beef } from "@/types"
+import { getAddressOrEnsName } from "@/utils"
 import { publicClient } from "@/utils/chain"
 import { queryKeys } from "./queryKeys"
 
-export const useEnsNames = (addresses: (Address | undefined)[]) => {
+export const useEnsNames = (addresses?: Address[]) => {
   return useQuery({
-    queryKey: ["ensName", ...addresses],
+    queryKey: ["ensName", addresses],
     queryFn: async () => {
-      const ensNameQueries = addresses.map((address) =>
-        address == null ? Promise.resolve(null) : getEnsName(ensConfig, { address }),
-      )
+      if (addresses === undefined) return undefined
+
+      const ensNameQueries = addresses.map((address) => getEnsName(ensConfig, { address }))
 
       const ensNames = await Promise.all(ensNameQueries)
 
       return ensNames
     },
-    enabled: addresses.length > 0,
+    enabled: addresses !== undefined,
   })
 }
 
@@ -51,6 +54,49 @@ export const useGetInfiniteBeefs = (pageSize: number, sort: BeefSortType, search
     queryFn: fetchBeefs,
     queryKey: [queryKeys.infiniteBeefs, sort, searchTitle],
     placeholderData: (previousData) => previousData,
+  })
+}
+
+const useGetAllUserDetailsAction = (addresses?: Address[]) =>
+  useQuery({
+    enabled: addresses !== undefined,
+    queryKey: [queryKeys.getAllUserDetails, addresses],
+    queryFn: () => getAllUserDetailsAction(addresses ?? []),
+  })
+
+const extractUsername = (details: { xHandle: string | null; email: string | null; farcasterId: string | null }) => {
+  return details?.xHandle ?? details?.email ?? details?.farcasterId ?? undefined
+}
+
+export const useGetUsernames = (beef?: Beef) => {
+  const addresses = beef ? [beef.owner, beef.challenger, ...beef.arbiters.map(({ address }) => address)] : undefined
+
+  const { data: ensNames } = useEnsNames(addresses)
+  const { data: userDetails } = useGetAllUserDetailsAction(addresses)
+
+  const getUsernames = () => {
+    if (userDetails === undefined || beef === undefined) return undefined
+
+    const addressToUsername = new Map(
+      userDetails.map((detail) => [detail.smartAccountAddress, extractUsername(detail)]),
+    )
+
+    // Null asserts, because compiler is not so smart :(
+    const [owner, challenger, ...arbiters] = addresses!.map((address, index) => {
+      return addressToUsername.get(address) ?? getAddressOrEnsName(address, ensNames?.[index], false)
+    })
+
+    return {
+      owner: owner!,
+      challenger: challenger!,
+      arbiters,
+    }
+  }
+
+  return useQuery({
+    enabled: beef !== undefined && ensNames !== undefined && userDetails !== undefined,
+    queryKey: [queryKeys.getUsernames, beef?.address],
+    queryFn: getUsernames,
   })
 }
 
